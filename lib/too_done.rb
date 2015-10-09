bc9ddca24cf91e00ff4f31f4a5a1bb2a5061525e
Joe Winter <joe@joeventures.com>
2015-10-09 15:38:38 -0400

    Started add(task)

--- lib/too_done.rb
+++ lib/too_done.rb
@@ -2,6 +2,8 @@ require "too_done/version"
 require "too_done/init_db"
 require "too_done/user"
 require "too_done/session"
+require "too_done/todo_list"
+require "too_done/task"
 
 require "thor"
 require "pry"
@@ -17,6 +19,13 @@ module TooDone
     def add(task)
       # find or create the right todo list
       # create a new item under that list, with optional date
+      binding.pry
+      list = TodoList.find_or_create_by(user_id: current_user, name: options[:list])
+      list = list[:id]
+      added_task = Task.create(list: list,
+                               due_date: options[:date],
+                               name: task,
+                               completed: false)
     end
 
     desc "edit", "Edit a task from a todo list."
--- lib/too_done/todo_list.rb
+++ lib/too_done/todo_list.rb
@@ -1,5 +1,6 @@
 module TooDone
   class TodoList < ActiveRecord::Base
     belongs_to :users
+    has_many :tasks
   end
 end
